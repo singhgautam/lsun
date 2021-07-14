22b4b8cc558e96f771967f23dbd37f0eb5e292d9
gautam <singh.gautam.iitg@gmail.com>
2021-07-14 10:56:06 -0400

    lsun

--- data.py
+++ data.py
@@ -39,14 +39,16 @@ def export_images(db_path, out_dir, flat=False, limit=-1):
         cursor = txn.cursor()
         for key, val in cursor:
             if not flat:
-                image_out_dir = join(out_dir, '/'.join(key[:6]))
+                image_out_dir = join(out_dir, '/'.join(key[:6].decode()))
             else:
                 image_out_dir = out_dir
             if not exists(image_out_dir):
                 os.makedirs(image_out_dir)
-            image_out_path = join(image_out_dir, key + '.webp')
-            with open(image_out_path, 'w') as fp:
-                fp.write(val)
+            print('Current key:', key)
+            image_out_path = join(image_out_dir, key.decode() + '.png')
+            img = cv2.imdecode(
+                numpy.fromstring(val, dtype=numpy.uint8), 1)
+            cv2.imwrite(image_out_path, img)
             count += 1
             if count == limit:
                 break
